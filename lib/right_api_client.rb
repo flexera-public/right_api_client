8f263774f865eaf107c687ebd4a88aaf39855800
Jake Douglas <jakecdouglas@gmail.com>
2010-11-04 18:55:08 -0700

    moar

--- lib/right_api_client.rb
+++ lib/right_api_client.rb
@@ -1,25 +1,25 @@
 require 'rubygems'
+# requires rest client 1.6.1
 require 'rest_client'
 require 'logger'
 require 'json'
+require 'set'
 require 'pp'
 
 RestClient.log = Logger.new(STDOUT)
 
 class RightApiClient
-  def initialize
+  def initialize(email, password, account_id)
+    @email, @password, @account_id = email, password, account_id
     @client = RestClient::Resource.new("https://moo.rightscale.com")
-    authorize()
-  rescue => e
-    pp e.response.body
-    raise e
+    @cookies = authorize()
   end
 
   def authorize
     params = {
-      'email'      => 'jake@rightscale.com',
-      'password'   => '',
-      'account_href' => '/api/accounts/71'
+      'email'        => @email,
+      'password'     => @password,
+      'account_href' => "/api/accounts/#{@account_id}"
     }
 
     response = @client['api/session'].post(params, 'X_API_VERSION' => 1.5) do |response, request, result, &block|
@@ -31,7 +31,7 @@ class RightApiClient
       end
     end
 
-    @cookies = response.cookies
+    response.cookies
   end
 
   def headers
@@ -39,19 +39,16 @@ class RightApiClient
   end
 
   def do_get(path, params={})
-    content_type, body = @client[path].get(headers.merge!('params' => params)) do |response, request, result, &block|
+    content_type, body = @client[path].get(headers.merge('params' => params)) do |response, request, result, &block|
       case response.code
       when 200
         [result.content_type, response.body]
       else
         raise "Wrong response #{response.code.to_s}"
-      end      
+      end
     end
 
     data = JSON.parse(body)
-    if data.kind_of?(Array)
-      data.reject!{|obj| obj.nil?}
-    end
 
     [data, content_type]
   end
@@ -77,15 +74,33 @@ class Resource
     "#<#{self.class.name} resource_type=\"#{resource_type}\"#{', name='+name.inspect if self.respond_to?(:name)}#{', resource_uid='+resource_uid.inspect if self.respond_to?(:resource_uid)}>"
   end
 
+  def reload
+    Resource.process(client, *client.do_get(href))
+  end
+
+  def define_instance_method(meth, &blk)
+    (class << self; self; end).module_eval do
+      define_method(meth, &blk)
+    end
+  end
+
+  def define_caching_association(meth, &blk)
+    define_instance_method(meth) do |*args|
+      view = args.first || 'default'
+      view = view.to_s
+      instance_variable_get("@#{meth.to_s}_#{view}") || instance_variable_set("@#{meth.to_s}_#{view}", blk.call(*args))
+    end
+  end
+
   def initialize(client, hash, content_type)
     @client = client
     @content_type = content_type
     @raw = hash
-    @attributes, @associations, @actions = [], [], []
+    @attributes, @associations, @actions = Set.new, Set.new, Set.new
     links = hash.delete('links') || []
     raw_actions = hash.delete('actions') || []
 
-    self_index = links.each_with_index do |link, idx|
+    self_index = links.any? && links.each_with_index do |link, idx|
       if link['rel'] == 'self'
         break idx
       end
@@ -99,61 +114,46 @@ class Resource
       hash['href'] = links.delete_at(self_index)['href']
     end
 
-    singleton = (class << self; self; end)
-
     attributes << :links
-    singleton.module_eval do
-      define_method(:links) do
-        return links
-      end
-    end
+    define_instance_method(:links) { return links }
 
     raw_actions.each do |action|
       action_name = action['rel']
       actions << action_name.to_sym
 
-      singleton.module_eval do
-        define_method(action_name) do
-          raise "need to implement actions"
-        end
-      end
-    end
-
-    hash.each do |k,v|
-      if v.kind_of?(Array) && v.any? && v.first.kind_of?(Hash)
-        associations << k.to_sym
-
-        singleton.module_eval do
-          define_method(k) { Resource.process(client, v, nil) }
-        end
-      else
-        attributes << k.to_sym
-
-        singleton.module_eval do
-          define_method(k) { return v }
-        end
+      define_instance_method(action_name) do
+        raise "need to implement actions"
       end
     end
 
     links.each do |link|
       associations << link['rel'].to_sym
 
-      singleton.module_eval do
-        define_method(link['rel']) do
-          Resource.process(client, *client.do_get(link['href']))
-        end
+      define_caching_association(link['rel']) do |*args|
+        view = args.first || :default
+        Resource.process(client, *client.do_get(link['href'], :view => view))
       end
     end
 
-    @resource_type = @content_type.scan(/\.com\.(.*)\+json/)[0][0]
+    hash.each do |k,v|
+      if associations.include?(k.to_sym)
+        define_caching_association(k) { Resource.process(client, v, nil) }
+      else
+        attributes << k.to_sym
+        define_instance_method(k) { return v }
+      end
+    end
+
+    if @content_type
+      @resource_type = @content_type.scan(/\.com\.(.*)\+json/)[0][0]
+    end
 
     # the api doesnt tell us what resources are supported by what clouds
     if resource_type =~ /cloud/
-      singleton.module_eval do
-        [:instances, :images].each do |rtype|
-          define_method(rtype) do
-            Resource.process(client, *client.do_get(href + "/#{rtype.to_s}"))
-          end
+      [:instances, :images, :ip_addresses, :volumes].each do |rtype|
+        define_caching_association(rtype) do |*args|
+          view = args.first || :default
+          Resource.process(client, *client.do_get(href + "/#{rtype.to_s}", :view => view))
         end
       end
     end
@@ -165,9 +165,4 @@ if __FILE__ == $0
   client = RightApiClient.new
   clouds = client.clouds
   instances = clouds.first.instances
-  begin
-    pp client.instances_for_cloud(232).map {|i| Resource.new(client, i) }
-  rescue => e
-    p e.response.body
-  end
 end
