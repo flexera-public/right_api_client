7d2a739d6a63d9c640666afa23a69d26c8b5f62a
John Mettraux <jmettraux@gmail.com>
2011-08-24 13:19:32 +0900

    preventing already initialized VERSION

--- lib/right_api_client/client.rb
+++ lib/right_api_client/client.rb
@@ -59,7 +59,7 @@ require 'json'
 require 'set'
 require 'cgi'
 
-require 'right_api_client/version'
+require 'right_api_client/version' unless defined?(RightApi::Client::VERSION)
 require 'right_api_client/helper'
 require 'right_api_client/resource'
 require 'right_api_client/resource_detail'
